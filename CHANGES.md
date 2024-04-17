04473bcc2506b32fa094979055d74e5dde8f95d2
Jacky Jiang <t83714@gmail.com>
2024-04-17 17:05:37 +1000

    add test cases for checking internal storage resource links

--- CHANGES.md
+++ CHANGES.md
@@ -1,18 +1,19 @@
 # 3.0.0
 
-- Upgrade nodejs to version 18
-- Upgrade to Magda minion SDK v3
+-   Upgrade nodejs to version 18
+-   Upgrade to Magda minion SDK v3
+-   Fixed support for checking internal storage resource links
 
 # 2.0.0
 
-- Upgrade nodejs to version 14
-- Upgrade other dependencies
-- Release all artifacts to GitHub Container Registry (instead of docker.io & https://charts.magda.io)
-- Upgrade magda-common chart version
-- Upgrade api to batch/v1 to be compatible with k8s 1.25 (now requires >=1.21)
+-   Upgrade nodejs to version 14
+-   Upgrade other dependencies
+-   Release all artifacts to GitHub Container Registry (instead of docker.io & https://charts.magda.io)
+-   Upgrade magda-common chart version
+-   Upgrade api to batch/v1 to be compatible with k8s 1.25 (now requires >=1.21)
 
 # 1.0.0
 
-- Upgrade dependencies
-- Upgrade CI scripts
-- Related to https://github.com/magda-io/magda/issues/3229, Use magda-common for docker image related logic
+-   Upgrade dependencies
+-   Upgrade CI scripts
+-   Related to https://github.com/magda-io/magda/issues/3229, Use magda-common for docker image related logic
---
+++ src/test/testStorageUrl.spec.ts
@@ -0,0 +1,172 @@
+import {} from "mocha";
+import { expect } from "chai";
+import sinon from "sinon";
+import nock from "nock";
+import _ from "lodash";
+import Ajv from "ajv";
+import urijs from "urijs";
+
+import { Record, AuthorizedRegistryClient } from "@magda/minion-sdk";
+import { encodeURIComponentWithApost } from "@magda/utils";
+
+import onRecordFound from "../onRecordFound.js";
+import { BrokenLinkAspect } from "../brokenLinkAspectDef.js";
+import {
+    setDefaultDomainWaitTime,
+    getDefaultDomainWaitTime
+} from "../getUrlWaitTime.js";
+
+const defaultStorageApiBaseUrl = "http://storage-api/v0";
+const defaultDatasetBucketName = "magda-datasets";
+const schema = require("@magda/registry-aspects/source-link-status.schema.json");
+
+describe("Test Internal Storage URL", function (this: Mocha.Suite) {
+    this.timeout(20000);
+    nock.disableNetConnect();
+    const registryUrl = "http://example.com";
+    const secret = "secret!";
+    const registry = new AuthorizedRegistryClient({
+        baseUrl: registryUrl,
+        jwtSecret: secret,
+        userId: "1",
+        maxRetries: 0,
+        tenantId: 1
+    });
+    let registryScope: nock.Scope;
+    const orignalDefaultDomainWaitTime: number = getDefaultDomainWaitTime();
+
+    before(() => {
+        // --- set default domain wait time to 0 second (i.e. for any domains that has no specific setting)
+        // --- Otherwise, it will take too long to complete property tests
+        setDefaultDomainWaitTime(0);
+        sinon.stub(console, "info");
+        nock.disableNetConnect();
+        nock.emitter.on("no match", onMatchFail);
+    });
+
+    const onMatchFail = (req: any, interceptor: any) => {
+        console.error(
+            `Match failure. \n Req: ${req.method} ${req.host}${req.path} \n interceptor: ${interceptor.href}`
+        );
+    };
+
+    after(() => {
+        setDefaultDomainWaitTime(orignalDefaultDomainWaitTime);
+
+        (console.info as any).restore();
+
+        nock.emitter.removeListener("no match", onMatchFail);
+    });
+
+    beforeEach(() => {
+        registryScope = nock(registryUrl); //.log(console.log);
+    });
+
+    afterEach(() => {
+        nock.cleanAll();
+    });
+
+    /**
+     * Generator-driven super-test: generates records and runs them through the
+     * onRecordFound function, listening for HTTP and FTP calls made and returning
+     * success or failure based on generated outcomes, then checks that they're
+     * recorded on a by-distribution basis as link status as well as on a by-record
+     * basis as a part of dataset quality.
+     */
+    it("Should correctly record link statuses", async () => {
+        const ajv = new Ajv();
+        const validate = ajv.compile(schema);
+
+        const testRecord: Record = {
+            id: "ds-1",
+            name: "Test Record",
+            sourceTag: "test-source",
+            tenantId: 1,
+            aspects: {
+                "dataset-distributions": {
+                    distributions: [
+                        {
+                            id: "dist-1",
+                            aspects: {
+                                "dcat-distribution-strings": {
+                                    downloadURL:
+                                        "magda://storage-api/ds-1/dist-1/test-file1.pdf"
+                                }
+                            }
+                        },
+                        {
+                            id: "dist-2",
+                            aspects: {
+                                "dcat-distribution-strings": {
+                                    accessURL:
+                                        "magda://storage-api/ds-1/dist-2/test-file2.pdf"
+                                }
+                            }
+                        }
+                    ]
+                }
+            }
+        };
+
+        const defaultStorageApiBaseUri = urijs(defaultStorageApiBaseUrl);
+
+        const storageApiScope = nock(
+            defaultStorageApiBaseUri.clone().path("").toString()
+        );
+
+        storageApiScope
+            .head(
+                `${defaultStorageApiBaseUri.path()}/${defaultDatasetBucketName}/ds-1/dist-1/test-file1.pdf`
+            )
+            .query(true)
+            .reply(200);
+
+        storageApiScope
+            .head(
+                `${defaultStorageApiBaseUri.path()}/${defaultDatasetBucketName}/ds-1/dist-2/test-file2.pdf`
+            )
+            .query(true)
+            .reply(200);
+
+        ["dist-1", "dist-2"].forEach((distId) => {
+            registryScope
+                .put(
+                    `/records/${encodeURIComponentWithApost(
+                        distId
+                    )}/aspects/source-link-status?merge=true`,
+                    (body: BrokenLinkAspect) => {
+                        const validationResult = validate(body);
+                        if (!validationResult) {
+                            throw new Error(
+                                "Json schema validation error: \n" +
+                                    validate.errors
+                                        .map(
+                                            (error) =>
+                                                `${error.dataPath}: ${error.message}`
+                                        )
+                                        .join("\n")
+                            );
+                        }
+                        console.log(body);
+                        expect(body.status).to.equal("active");
+                        return true;
+                    }
+                )
+                .reply(201);
+        });
+
+        await onRecordFound(
+            testRecord,
+            registry,
+            defaultStorageApiBaseUrl,
+            defaultDatasetBucketName,
+            0,
+            0,
+            {},
+            {}
+        );
+
+        registryScope.done();
+        storageApiScope.done();
+    });
+});
